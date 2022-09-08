7280026d4ca9b52a4e05bbc9eb2b5a66e11a3246
baba-s <shotababa1206@gmail.com>
2022-09-08 19:41:43 +0900

    add

--- Runtime/UniTaskWhenAllExtensionMethods.cs
+++ Runtime/UniTaskWhenAllExtensionMethods.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 
 namespace Kogane
@@ -6,6 +6,11 @@ namespace Kogane
     public static class UniTaskWhenAllExtensionMethods
     {
         public static UniTask WhenAll( this IEnumerable<UniTask> self )
+        {
+            return UniTask.WhenAll( self );
+        }
+
+        public static UniTask<T[]> WhenAll<T>( this IEnumerable<UniTask<T>> self )
         {
             return UniTask.WhenAll( self );
         }
